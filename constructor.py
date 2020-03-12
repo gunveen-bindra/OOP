085c4cd868c7facb8c615cf6520aeb8f810affce
gunveen-bindra <60962476+gunveen-bindra@users.noreply.github.com>
2020-03-12 21:08:26 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,14 @@
+# Program to show constructor
+
+class Person:
+    def __init__(self,fname,lname):
+        self.fname=fname
+        self.lname=lname
+    def getFullName(self):
+        print(self.fname," ",self.lname)
+
+
+p1=Person("Rohit","Sharma")
+p1.getFullName()
+p2=Person("Riya","Sharma")
+p2.getFullName()
